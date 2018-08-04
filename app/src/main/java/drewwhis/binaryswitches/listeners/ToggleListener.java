44efd8a79a07d1ffa9ca06e80b86a3fe54d3042b
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2018-08-04 12:17:02 -0500

    Add comments

--- app/src/main/java/drewwhis/binaryswitches/listeners/ToggleListener.java
+++ app/src/main/java/drewwhis/binaryswitches/listeners/ToggleListener.java
@@ -5,11 +5,21 @@ import android.widget.CompoundButton;
 import drewwhis.binaryswitches.ui.activities.GameActivity;
 import drewwhis.binaryswitches.ui.views.ToggleLabelViewGroup;
 
+/**
+ * A listener to update the value of a GameActivity when a ToggleLabelViewGroup changes state.
+ */
 public class ToggleListener implements CompoundButton.OnCheckedChangeListener {
   private final GameActivity mActivity;
   private final ToggleLabelViewGroup mGroup;
 
-  public ToggleListener(GameActivity activity, ToggleLabelViewGroup group) {
+  /**
+   * Creates a ToggleListener for a ToggleLabelViewGroup in the context of an activity.
+   * @param activity Activity that the listener references.
+   * @param group Group to which the listener is applied.
+   * @throws IllegalArgumentException Listener and activity cannot be null.
+   */
+  public ToggleListener(GameActivity activity, ToggleLabelViewGroup group)
+      throws IllegalArgumentException {
     if (activity == null || group == null) {
       throw new IllegalArgumentException("Activity and Group cannot be null.");
     }
@@ -18,6 +28,11 @@ public class ToggleListener implements CompoundButton.OnCheckedChangeListener {
     mGroup = group;
   }
 
+  /**
+   * Updates the value in the activity with the bit value of the ToggleLabelViewGroup.
+   *
+   * {@inheritDoc}
+   */
   @Override
   public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
     int currentValue = mActivity.getCurrentValue();
